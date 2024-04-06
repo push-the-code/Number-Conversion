051b65a6ebcd18a2297d1f57059d9532d69c05e1
PushTheCode <43913828+push-the-code@users.noreply.github.com>
2024-04-06 19:33:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def bin_to_dec (str):
-    dec = 0
-    for i in range(len(str)):
-        num = int(str[i])
-        dec += num * pow(2, i)
-    print(dec)
+class Number_Conversion:
+    def bin_to_dec (str):
+        dec = 0
+        for i in range(len(str)):
+            num = int(str[i])
+            dec += num * pow(2, i)
+        print(dec)
     
-bin_to_dec("110011")
+    def dec_to_bin(int):
+        bin = ""
+        while int > 0:
+            bin += str(int % 2)
+            int //= 2
+        print(bin)
+    
+n = Number_Conversion
