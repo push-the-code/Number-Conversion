cae046931eff938b57a1268ba2bf1adeed92241a
PushTheCode <43913828+push-the-code@users.noreply.github.com>
2024-04-06 17:47:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-binary = "1111"
-dec = 0
-
-for i in range(len(binary)):
-    num = int(binary[i])
-    dec = dec + num * pow(2, i)
+def bin_to_dec (str):
+    dec = 0
+    for i in range(len(str)):
+        num = int(str[i])
+        dec += num * pow(2, i)
+    print(dec)
     
-    
-print(dec)
+bin_to_dec("110011")
